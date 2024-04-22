79fa38b8d882a3133cac53be386804a0ab5de28c
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-04-22 17:20:45 +0800

    update file input output

--- pertemuan03/InputOutput.java
+++ pertemuan03/InputOutput.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class InputOutput {
 	public static void main(String[] args) {
-		int nilai = 0;
-		double rata = 0;
-		String nama = "Yasser";
 		Scanner scanner = new Scanner(System.in);
+		String nama;
+		int jumlahSks;
+		double IPK;
 
-		System.out.println("Nama: " + nama);
-		
-		System.out.print("Input nama baru: ");
+		System.out.print("Masukkan nama anda: ");
 		nama = scanner.nextLine();
-		System.out.println("Nama baru: " + nama);
+		System.out.println("Hai, " + nama);
+
+		System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+		jumlahSks = scanner.nextInt();
+
+		System.out.print("Masukkan IPK sementara: ");
+		IPK = scanner.nextDouble();
+
+		double jumlah = IPK * jumlahSks;
+		System.out.println("Total nilai = " + jumlah);
 	}
 }
